71131ac4145fff1f384906a3192a9b2b014acd57
melanie caro <melanie.caro@stackerine.com>
2019-12-03 15:25:53 +0100

    create a file and write in with Node

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ fs.readFile('./content.js', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+fs.writeFile(
+  'contentCreateWithNode.js',
+  'coucou !',
+  { encoding: 'utf8', flag: 'w' },
+  err => {
+    if (err) throw err;
+    console.log('you write in the file');
+  }
+);
+
+// fs.readFile('./contentCreateWithNode.js');
